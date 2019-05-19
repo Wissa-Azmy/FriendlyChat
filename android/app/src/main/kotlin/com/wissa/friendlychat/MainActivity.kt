16236f9f0ff32a3913b5759d55fa03203e7df7ec
Wissa Azmy <wissa.azmy@gmail.com>
2019-05-19 12:59:54 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/wissa/friendlychat/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wissa.friendlychat
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
