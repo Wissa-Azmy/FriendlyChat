d835a66d646d9abd047e86d9fa68bf43367d55f7
Wissa Azmy <wissa.azmy@gmail.com>
2019-05-19 20:02:17 +0200

    Add UI animation to chat msgs display

--- lib/main.dart
+++ lib/main.dart
@@ -20,17 +20,23 @@ class ChatScreen extends StatefulWidget {
   State<StatefulWidget> createState() => ChatScreenState();
 }
 
-
-
-class ChatScreenState extends State<ChatScreen> {
+// The vsync prevents animations that are offscreen from consuming unnecessary resources.
+// To use your ChatScreenState as the vsync include a TickerProviderStateMixin mixin
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final _textFieldController = TextEditingController();
   final _messages = <ChatMessage>[];
 
   _handleSubmission(String text){
     _textFieldController.clear();
+
+    AnimationController animation = AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+    ChatMessage message = ChatMessage(text: text, animationController: animation,);
+
     setState(() {
-      _messages.insert(0, ChatMessage(text: text));
+      _messages.insert(0, message);
     });
+
+    message.animationController.forward();
   }
 
   _buildTextFieldComposer() {
@@ -73,7 +79,7 @@ class ChatScreenState extends State<ChatScreen> {
             child: ListView.builder(
               itemBuilder: (_, index) => _messages[index],
               itemCount: _messages.length,
-              reverse: true,
+              reverse: true, //to make the ListView start from the bottom of the screen
               padding: EdgeInsets.all(8.0),
             ),
           ),
@@ -86,38 +92,52 @@ class ChatScreenState extends State<ChatScreen> {
       )
     );
   }
+
+  // Dispose of your animation controllers to free up your resources when they are no longer needed which is required only in single screens apps
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
+  }
 }
 
 
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
+  ChatMessage({this.text, this.animationController});
   final String text;
+  final AnimationController animationController;
   static const String _name = "Wissa Michael";
+
   
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(right: 16.0),
-            child: CircleAvatar(child: Text(_name[0]),), // Show the first char of the name
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(_name, style: Theme.of(context).textTheme.subhead),
-              Container(
-                margin: EdgeInsets.only(top: 5.0),
-                child: Text(text),
-              )
-            ],
-          )
-        ],
+    return SizeTransition(
+      sizeFactor: CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0]),), // Show the first char of the name
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(_name, style: Theme.of(context).textTheme.subhead),
+                Container(
+                  margin: EdgeInsets.only(top: 5.0),
+                  child: Text(text),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
