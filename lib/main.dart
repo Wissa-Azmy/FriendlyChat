143fb7df996bec9361e65c8cccce6b20cde44588
Wissa Azmy <wissa.azmy@gmail.com>
2019-05-19 18:50:44 +0200

    Add UI for displaying msgs

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,24 @@ class FriendlyChat extends StatelessWidget {
   }
 }
 
+
+
 class ChatScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => ChatScreenState();
 }
 
+
+
 class ChatScreenState extends State<ChatScreen> {
   final _textFieldController = TextEditingController();
+  final _messages = <ChatMessage>[];
 
   _handleSubmission(String text){
     _textFieldController.clear();
+    setState(() {
+      _messages.insert(0, ChatMessage(text: text));
+    });
   }
 
   _buildTextFieldComposer() {
@@ -59,7 +67,58 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text("Friendly Chat"),
       ),
-      body: _buildTextFieldComposer(),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              itemBuilder: (_, index) => _messages[index],
+              itemCount: _messages.length,
+              reverse: true,
+              padding: EdgeInsets.all(8.0),
+            ),
+          ),
+          Divider(height: 1.0,),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextFieldComposer(),
+          )
+        ],
+      )
+    );
+  }
+}
+
+
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  static const String _name = "Wissa Michael";
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0]),), // Show the first char of the name
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_name, style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
